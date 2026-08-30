113d9751533fdf40b92dd3614dc4da1657d1ae5e
Davi Guedes <daviguedes269@gmail.com>
2026-08-29 21:22:52 -0300

    feat: configura projeto Django com MySQL

---
+++ produtos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Categoria, Pizza
+
+admin.site.register(Categoria)
+admin.site.register(Pizza)    
+# Register your models here.
---
+++ produtos/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Pizza(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        related_name="pizzas"
+    )
+
+    def __str__(self):
+        return self.nome
